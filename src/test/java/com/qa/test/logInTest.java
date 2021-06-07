729ac6ce420172fd8cfa2e307e7024e7d419f980
Abdul <aqadir94@gmail.com>
2021-06-06 20:53:25 -0400

    commit

--- src/test/java/com/qa/test/logInTest.java
+++ src/test/java/com/qa/test/logInTest.java
@@ -1,5 +1,8 @@
 package com.qa.test;
 
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.qa.base.BaseTest;
@@ -8,6 +11,7 @@ import com.qa.pages.LogInPage;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidElement;
 
 public class logInTest extends BaseTest {
@@ -25,7 +29,18 @@ public class logInTest extends BaseTest {
 		DriverManager.getDriver().findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector()."
 				+"scrollable(true)).scrollIntoView(new UiSelector().text(\"Sauce Labs Fleece Jacket\"))"));
 		
-		System.out.println(DriverManager.getDriver().toString());
+		
+		MobileElement element = (MobileElement)DriverManager.getDriver().findElement(By.id("Sauce Labs Fleece Jacket"));
+		
+		String elementID = element.getId();
+		  HashMap<String, String> scrollObject = new HashMap<String, String>();
+		  scrollObject.put("element", elementID);
+//		  scrollObject.put("direction", "down");
+//		  scrollObject.put("predicateString", "label == 'ADD TO CART'");
+//		  scrollObject.put("name", "test-ADD TO CART");
+		  scrollObject.put("toVisible", "sdfnjksdnfkld");  //the value here doesn't matter
+		  DriverManager.getDriver().executeScript("mobile:scroll", scrollObject);
+
 	}
 	
 	
